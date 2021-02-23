01e4e671ee4b76fb25bc2363e1c7991bc5394397
normalFace <1343741207@qq.com>
2021-02-23 22:38:29 +0800

    20210223

--- app/src/main/java/com/sssstar/toolman/MainActivity2.java
+++ app/src/main/java/com/sssstar/toolman/MainActivity2.java
@@ -3,9 +3,12 @@ package com.sssstar.toolman;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity2 extends AppCompatActivity {
+    private String TAG = "MainActivity2";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +17,11 @@ public class MainActivity2 extends AppCompatActivity {
         Intent intent = new Intent();
         intent.putExtra("result","this is MainActivity2");
         setResult(1,intent);
+
+        Intent intent_get = getIntent();
+        Uri intent_data = intent_get.getData();
+        Bundle intent_extras = intent_get.getExtras();
+        Log.i(TAG,"intent_data:"+intent_data.toString());
+        Log.i(TAG,"intent_extras:"+intent_extras.toString());
     }
 }
